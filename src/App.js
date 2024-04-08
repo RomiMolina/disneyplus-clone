d35233896bed4f173fdacfb0c3c4b55a2f15adb1
RomiMolina <roomi.molina@gmail.com>
2024-04-08 16:57:26 -0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import './App.css';
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
      <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Login/>}/>
        
