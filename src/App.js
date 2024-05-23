35f777d4d7a90fb741574442e56f1697c7b8a1cf
RomiMolina <roomi.molina@gmail.com>
2024-05-23 13:09:37 -0300

    create carousel slick

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import './App.css';
 import Login from "./components/Login";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Login/>}/>
-       
+        <Route path="/home" element={<Home/>}/>
       </Routes>
      </Router>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,31 @@
 
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector} from 'react-redux';
 import { useNavigate } from "react-router-dom";
-import { auth, provider, firebaseApp } from "../firebase";
-import { selectUserEmail, selectUserName , selectUserPhoto, setUserLoginDetails} from '../features/user/userSlice'
+import { auth, provider } from "../firebase";
+import { selectUserName , selectUserPhoto, setSignOutState, setUserLoginDetails} from '../features/user/userSlice'
+
 
 
 const Header = () => {
     const dispatch = useDispatch();
     const history = useNavigate();
     const userName = useSelector(selectUserName);
-    const userEmail = useSelector(selectUserEmail);
     const userPhoto = useSelector(selectUserPhoto);
 
-    const setUser = (user) => {
-        dispatch(setUserLoginDetails({
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-        }));
-    }
-      
+    useEffect(()=> {
+        auth.onAuthStateChanged(async (user)=>{
+            if(user) {
+                setUser(user);
+                history("/home")
+            }
+        });
+    },[userName]);
+   
     const handleAuth = () => {
-        auth
+        if(!userName){
+            auth
           .signInWithPopup(provider)
           .then((result) => {
             console.log(result.user);
@@ -31,8 +34,25 @@ const Header = () => {
           .catch((error) => {
             alert(error.message);
           });
+        } else if (userName){
+            auth
+            .signOut()
+            .then(()=>{
+                dispatch(setSignOutState());
+                history("/")
+            })
+            .catch((err) => alert(err.message));
+        }
       };
 
+      const setUser = (user) => {
+        dispatch(setUserLoginDetails({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+        }));
+    }
+      
 
   return (
     <Nav>
@@ -70,7 +90,12 @@ const Header = () => {
                 <span>SERIES</span>
             </a>
         </NavMenu>
-        <UserImg img={userPhoto} alt={userName} />
+        <SignOut>
+        <UserImg src={userPhoto} alt={userName} />
+        <DropDown>
+            <span onClick={handleAuth}>Sign out</span>
+        </DropDown>
+        </SignOut>
         </>
         )}
         
@@ -194,7 +219,45 @@ transition: all 0.2s ease 0;
 }
 `
 const UserImg = styled.img`
-    height: 100%;
+    height: 48px;
+    width: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
+const DropDown = styled.div`
+position: absolute;
+top: 48px;
+right: 0px;
+background: rgb(19, 19, 19);
+border: 1px solid rgba(151, 151, 151, 0.34);
+border-radius: 4px;
+box-sahdow: rgb(0 0 0 / 50%) 0px 0px 18px 0px; 
+padding: 10px;
+font-size: 14px;
+letter-spacing: 3px;
+width: 100px;
+opacity: 0;
+`
+const SignOut = styled.div`
+position: relative;
+height: 48px;
+width: 48px;
+display: flex;
+cursor: pointer;
+align-items: center;
+justify-content: center;
 
+${UserImg}{
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+}
+
+&:hover{
+    ${DropDown} {
+        opacity: 1;
+        transition-duration: 1s;
+    }
+}
+`
 export default Header
