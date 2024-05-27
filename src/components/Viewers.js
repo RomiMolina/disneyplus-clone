b1a1a8fefe5da647487a101ffe65a944a79960ed
RomiMolina <roomi.molina@gmail.com>
2024-05-27 12:35:02 -0300

    add videos and hover

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,18 +5,33 @@ const Viewers = (props) =>{
         <Container>
             <Wrap>
                 <img src="/images/viewers-disney.png" alt="viewDisney" />
+                <video autoPlay={true} loop={true} playsInline muted >
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-pixar.png" alt="viewPixar" />
+                <video autoPlay={true} loop={true} playsInline muted >
+                    <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-marvel.png" alt="viewMarvel" />
+                <video autoPlay={true} loop={true} playsInline muted  >
+                    <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-starwars.png" alt="viewSW" />
+                <video autoPlay={true} loop={true} playsInline muted  >
+                    <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-national.png" alt="viewNat" />
+                <video autoPlay={true} loop={true} playsInline muted  >
+                    <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4" />
+                </video>
             </Wrap>
         </Container>
     )
@@ -57,6 +72,26 @@ img {
     width: 100%;
     z-index: 1;
     top: 0;
+}
+
+video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
+}
+&:hover {
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+    transform: scale(1.05);
+    border-color: rgba( 249, 249, 249, 0.8);
+
+    video {
+        opacity: 1;
+    }
 }
 `;
 
